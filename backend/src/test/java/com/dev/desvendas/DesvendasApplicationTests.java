d11ac95d1b88801a4f4d9727279b4126c6cc6762
thiagopazjs <thiagopaz907@gmail.com>
2021-11-04 14:24:32 -0300

    first commit

---
+++ backend/src/test/java/com/dev/desvendas/DesvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.desvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
