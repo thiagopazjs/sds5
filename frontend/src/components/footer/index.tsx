a3b5a66a036d04e351ef9f810b6d6c7d207d332b
thiagopazjs <thiagopaz907@gmail.com>
2021-11-07 14:34:02 -0300

    Pagination

--- frontend/src/components/footer/index.tsx
+++ frontend/src/components/footer/index.tsx
@@ -3,8 +3,8 @@ const Footer = () => {
         <footer className="footer mt-auto py-3 bg-dark">
         <div className="container">
           <p className="text-light">App desenvolvido por <a href="https://github.com/thiagopazjs" target="_blank" rel="noreferrer">Thiago Paz</a></p>
-          <p className="text-light"><small><strong> Spring React</strong><br/>
-            Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+          <p className="text-light"><small><strong> Com as Tecnologias: Spring, React</strong><br/>
+            Evento promovido pela escola DevSuperior: </small></p>
         </div>
       </footer>
     );
